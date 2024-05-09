3c38602cfe59ef789631d55214cb6b489025db20
Jainik Darji <darjijainik@gmail.com>
2024-05-10 02:37:19 +0530

    complete contact page and completly enter message in contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,22 +1,50 @@
 // import React from 'react'
 import "./Contact.css";
-import theme_pattern from "../../assets/theme_pattern.svg";
+// import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "f8bdfb76-d838-4677-8834-7c7afe418a40");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message)
+        }
+      };
+
+
+
+
   return (
     <div>
-      <div className="contact">
+      <div id="contact" className="contact">
         <div className="contact-title">
           <h1>Get in Touch</h1>
-          <img src={theme_pattern} alt="" />
+          {/* <img src={theme_pattern} alt="" /> */}
         </div>
         <div className="contact-section">
           <div className="contact-left">
-            <h1>Let's Talk</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <h1>Let s Talk</h1>
+            <p>Connect with us effortlessly through our user-friendly contact page. Whether you have questions,  or inquiries, us me is here to assist you promptly. Simply fill out the form provided, and we get back to you swiftly. Your satisfaction is our priority</p>
             <div className="contact-details">
               <div className="contact-detail">
                 <img src={mail_icon} alt="" />
@@ -32,17 +60,17 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <form className="contact-right">
-            <label htmlFor="">Your Name</label>
+          <form onSubmit={onSubmit} className="contact-right">
+            <label htmlFor=""> </label>
             <input type="text" placeholder="Enter Your Name" name="name" />
-            <label htmlFor="">Your Email</label>
+            <label htmlFor=""></label>
             <input
               type="text"
               placeholder="Enter Your Email"
               name="email"
               id=""
             />
-            <label htmlFor="">Write your message here</label>
+            <label htmlFor=""></label>
             <textarea
               name="message"
               rows="8"
