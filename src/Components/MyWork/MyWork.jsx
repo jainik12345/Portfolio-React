762a662614f39c13af8aa6689e73c7a8893a2216
Jainik Darji <darjijainik@gmail.com>
2024-05-11 19:54:25 +0530

    mywork section is complete

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,31 +1,33 @@
 // import React from 'react'
-import  './MyWork.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mywork_data from '../../assets/mywork_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import "./MyWork.css";
+// import theme_pattern from '../../assets/theme_pattern.svg'
+import mywork_data from "../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
 
 const MyWork = () => {
   return (
-    
-      <div className="mywork">
-        <div className="mywork-title">
-            <h1>My Latest Work</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-        <div className="mywork-container">
-            {mywork_data.map((work , index) =>{
-                return <img key={index} src={work.w_img} alt="" />
-            })}
-        </div>
-        <div className="mywork-showmore">
-            <p>
-                Show More
-            </p>
-            <img src={arrow_icon} alt="" />
-        </div>
+    <div id="work" className="mywork">
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        {/* <img src={theme_pattern} alt="" /> */}
       </div>
-    
-  )
-}
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <>
+              <a href="https://github.com/jainik12345" target="_blank" >
+                <img key={index} src={work.w_img} alt="" />
+              </a>
+            </>
+          );
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default MyWork
+export default MyWork;
